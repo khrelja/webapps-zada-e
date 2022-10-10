b75748bf26443c9c08543d6bb7157badf5b4e3f4
1Keti1 <keti.hrelja@gmail.com>
2022-10-10 13:35:12 +0200

    gotov drugi zadatak

---
+++ zadatak2_Keti.js
@@ -0,0 +1,10 @@
+function rang(h){
+    if (h >= 100 & h<= 150000){
+        console.log ("Unutar  [100,150000] je")
+    }
+    else{
+        console.log("Nije unutar [100,150000]")
+    }
+}
+
+rang(420)
